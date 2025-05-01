fd08944a705aa355283c76abd42ead3ccd2462b7
P0NIZAVRIK <alex.baney131204@gmail.com>
2025-05-01 16:56:31 +0400

    ASDASD

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import Reg from "./components/Reg.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header/>
+  <Reg/>
+  <Footer/>
 </template>
 
 <style scoped>
